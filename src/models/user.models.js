16c5aa1a99cc1f04002871fb7e1b8dcade0a3aaa
Divy Koushik Mishra <divykoushikmishra@gmail.com>
2024-03-22 15:27:44 +0530

    Delete unused files, add MongoDB connection, and update dependencies

---
+++ src/models/user.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    usename: {
+      type: String,
+      required: [true, "Please provide a username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide an password"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+  },
+  {}
+);
+
+const User = mongoose.model.users || mongoose.model("users", userSchema);
+
+export default User;
